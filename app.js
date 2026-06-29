4e40813699f7668c58a5bd4361b34780dca08842
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-06-29 14:46:46 +0300

    build keyboard

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
-// smoke test 
-// alert("Your operation was successful!");
+let nameOfCurrentUser = "";
+let win = false;
+let lose = false;
+let gameTimeOut;
+let guessed = [];
+let remainingAttemps = 6;
+let word = "";
+
+// Additional
+let isDarkMode = false;
+let isMuted = false;
+
+
+// The words
+const Words = ['SPACEMAN', 'FOOTBALL', 'JAVASCRIPT', 'HTML', 'GENERAL', 'RELAX', 'MIND'];
+
+// The keyboard alphabet
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split("")
+
+// link the keyboard html to js
+const keyboardEl = document.querySelector('#keyboard');
+
+const wordsEl = document.querySelector('#words');
+
+function startGame() {
+
+    // To the game select random word
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
+    word = Words [ Math.floor ( Math.random() * Words.length ) ];
+    
+}
