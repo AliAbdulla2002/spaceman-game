245c24a39d14baeb3a8531ccd5b98ef8d2e6e726
AliAbdulla2002 <alisaleh.2002@gmail.com>
2026-06-30 09:28:54 +0300

    Use querySelectorAll to link keyboard and improve code

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+/*-------------------------------- Constants --------------------------------*/
+// The words
+const Words = ['PLANET', 'ROCKET', 'ORBITS', 'SPACEY', 'GALAXY','STARRY', 'PYTHON', 'CODING', 'FRIEND', 'BEAUTY'];
+
+/*-------------------------------- Variables --------------------------------*/
+
 let nameOfCurrentUser = "";
 let win = false;
 let lose = false;
 let gameTimeOut;
-let guessed = [];
+// let guessed = [];
 let remainingAttemps = 6;
 let word = "";
 
@@ -11,30 +17,49 @@ let isDarkMode = false;
 let isMuted = false;
 
 
-// The words
-const Words = ['PLANET', 'ROCKET', 'ORBITS', 'SPACEY', 'GALAXY','STARRY', 'PYTHON', 'CODING', 'FRIEND', 'BEAUTY'];
-
+/*------------------------ Cached Element References ------------------------*/
 // link the keyboard html to js
 const keyboardEl = document.querySelector('#keyboard');
-
 const wordsEl = document.querySelector('#words');
 
-function startGame() {
 
+function startGame() {
     // To the game select random word and make the line with the length of the words
     // here I research about the floor 👇
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
     word = Words [ Math.floor ( Math.random() * Words.length ) ];
+    
     // Make the dashs by word length 
     let emptylines = "";
-    for (let i = 0; i < word.length; i++) {
+    for (let i = 0; i < word.length; i++) 
+    {
         emptylines += "_ "
     }
     wordsEl.textContent = emptylines
 
-    console.log("The Secret words is: "+ word);
-    
+    console.log("The Secret words is: "+ word); // just a smoke test for the function, and if i want cheat 
 }
+
+// If I used lowerCase letters not working so should be capital as the words
+const TheLetters = document.querySelectorAll('#keyboard button');
+TheLetters.forEach(function(item) 
+{
+    item.addEventListener('click', function() 
+    {
+        let clickedLetter = item.textContent.toUpperCase(); 
+        item.disabled = true;
+        guess(clickedLetter);
+    });
+});
+
 
 startGame();
 
+
+
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+/*-------------------------------- Functions --------------------------------*/
